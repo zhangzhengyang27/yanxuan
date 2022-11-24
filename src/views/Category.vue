35250d3a8f1fc9a555af933f6938e9ea03691d64
zhangzhengyang <zhangzhengyang27@aliyun.com>
2022-11-24 13:02:38 +0800

    解决分类滚动的 bug

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -8,12 +8,13 @@
           :key="cate.id"
           :class="{active: actived === index}"
           @click="select(index)"
-        >{{cate.name}}</li>
+        >{{ cate.name }}
+        </li>
       </ul>
 
-      <ul class="cate-list">
+      <ul class="cate-list" ref="cate-list">
         <li class="cate-list-item" v-for="sub in categoryGroupList" :key="sub.id">
-          <div class="title">{{sub.name}}</div>
+          <div class="title">{{ sub.name }}</div>
           <ul class="sub-item">
             <li
               class="sub-item-detail"
@@ -21,17 +22,17 @@
               :key="item.id"
               @click="getSubList(item.id)"
             >
-              <img :src="item.prettyBannerUrl" alt class="ban" />
-              <div class="name">{{item.name}}</div>
+              <img :src="item.prettyBannerUrl" alt class="ban"/>
+              <div class="name">{{ item.name }}</div>
             </li>
           </ul>
         </li>
       </ul>
     </div>
 
-    <nav-footer />
+    <nav-footer/>
 
-    <loading v-show="isLoading" />
+    <loading v-show="isLoading"/>
   </div>
 </template>
 
@@ -39,6 +40,7 @@
 import NavFooter from '../components/NavFooter'
 import { API_CATE, API_CATE_SUB } from '@/api.config.js'
 import Loading from '../components/Loading'
+
 export default {
   data () {
     return {
@@ -48,7 +50,10 @@ export default {
       isLoading: false
     }
   },
-  components: { NavFooter, Loading },
+  components: {
+    NavFooter,
+    Loading
+  },
   watch: {
     actived () {
       this.getSubCategory(this.categoryList[this.actived].id)
@@ -58,7 +63,6 @@ export default {
     async getCategory () {
       this.isLoading = true
       const { categoryList } = await this.$axios.get(API_CATE)
-      // console.log(categoryList)
       this.categoryList = categoryList
       this.getSubCategory(categoryList[0].id)
       this.isLoading = false
@@ -67,12 +71,13 @@ export default {
       this.actived = index
     },
     async getSubCategory (categoryId) {
+      const scrollContainer = this.$refs['cate-list']
+      scrollContainer.scrollTop = '0px'
       const {
         category: { categoryGroupList }
       } = await this.$axios.post(API_CATE_SUB, {
         categoryId
       })
-      // console.log(categoryGroupList)
       this.categoryGroupList = categoryGroupList
     },
     getSubList (subId) {
@@ -96,10 +101,12 @@ export default {
   bottom: 0.97rem;
   background-color: $colorA;
   display: flex;
+
   &-nav {
     width: 1.4rem;
     padding: 0.2rem 0;
     margin-right: 0.2rem;
+
     &-item {
       height: 0.6rem;
       line-height: 0.6rem;
@@ -107,8 +114,10 @@ export default {
       margin-top: 0.4rem;
       color: $colorC;
     }
+
     .active {
       position: relative;
+
       &::before {
         content: '';
         position: absolute;
@@ -120,29 +129,36 @@ export default {
       }
     }
   }
+
   &-list {
     flex: 1;
     overflow-y: scroll;
     padding: 0.2rem;
     border-left: 1px solid $colorE;
+
     &-item {
       margin-bottom: 0.3rem;
+
       .title {
         color: $colorC;
         font-weight: 700;
         border-bottom: 1px solid $colorE;
       }
+
       .sub-item {
         display: flex;
         flex-wrap: wrap;
+
         &-detail {
           width: 1.4rem;
           margin: 0.4rem 0.4rem 0 0;
           text-align: center;
+
           .banner {
             width: 1.2rem;
             height: 1.2rem;
           }
+
           .name {
             font-size: $fontA;
           }
